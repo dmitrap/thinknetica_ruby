196dd29aa447f04578f6e4037f4a7133fec8e42c
dmitrap <dmitrap@gmail.com>
2018-05-17 22:33:57 +0500

    fixed tabs and tweaked right_triangle a lil'

--- lesson1/right_triangle.rb
+++ lesson1/right_triangle.rb
@@ -6,23 +6,24 @@ puts 'сторона b?'
 b = gets.to_f
 puts 'сторона c?'
 c = gets.to_f
-puts 'ваш треугольник равнобедренный' if a == b || a == c || b == c
 puts 'ваш треугольник равносторонний' if a == b && a == c
 if a > b
-	max = a
-	leg1 = b
+  max = a
+  leg1 = b
 else
-	max = b
-	leg1 = a
+  max = b
+  leg1 = a
 end
 if max > c
-	leg2 = c
+  leg2 = c
 else
-	leg2 = max
-	max = c
+  leg2 = max
+  max = c
 end
-if max**2 == leg1**2 + leg2**2
-	puts "ваш треугольник прямоугольный"
+if max**2 == leg1**2 + leg2**2 && (a == b || a == c || b == c)
+  puts "ваш треугольник прямоугольный и равнобедренный"
+elsif max**2 == leg1**2 + leg2**2
+  puts "ваш треугольник прямоугольный"
 else
-	puts "ваш треугольник не прямоугольный!"
+  puts "ваш треугольник не прямоугольный!"
 end
