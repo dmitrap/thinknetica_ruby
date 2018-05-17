bc41b6b55f3078b01bf1457dfeee6d4789226915
dmitrap <dmitrap@gmail.com>
2018-05-17 11:42:32 +0500

    update5

--- lesson1/lesson1-4.rb
+++ lesson1/lesson1-4.rb
@@ -5,13 +5,11 @@ c = gets.to_f
 d = b**2 - 4 * a * c
 if d < 0
   puts "Дискриминант D=#{d}, что меньше нуля. Корней нет!"
-else d >= 0
+elsif d > 0
   sqrt_d = Math.sqrt(d)
   x1 = (-b + sqrt_d) / (2 * a)
   x2 = (-b - sqrt_d) / (2 * a)
-  if x1 == x2
-    puts "Дискриминант D=#{d}, корень X=#{x1}"
-  else
-    puts "Дискриминант D=#{d}, корень X1=#{x1}, корень X2=#{x2}"
-  end
+  puts "Дискриминант D=#{d}, корень X1=#{x1}, корень X2=#{x2}"
+else x1 == x2
+  puts "Дискриминант D=#{d}, корень X=#{x1}"
 end
