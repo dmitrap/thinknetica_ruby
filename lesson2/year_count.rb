e3e01211d7da34e1f27fc36c2526183b9681a339
dmitrap <dmitrap@gmail.com>
2018-05-24 21:34:04 +0500

    more fixes: year_count.rb

--- lesson2/year_count.rb
+++ lesson2/year_count.rb
@@ -3,11 +3,10 @@ today = gets.to_i
 month = gets.to_i
 year = gets.to_i
 months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-months[0] = 0 if month == 1
 months[1] = 29 if year % 400 == 0 || year % 4 == 0 && year % 100 != 0
 day = 0
-months[0...month].each do |sum|
-  day += sum
+(0..month - 2).each do |sum|
+  day += months[sum]
 end
 today = today + day
 puts "сегодня #{today} день года!"
