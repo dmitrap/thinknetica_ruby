531e837c3f25377a15d2453cabb572939bbd8ae3
dmitrap <dmitrap@gmail.com>
2018-05-24 18:33:28 +0500

    more fixes: fibonacci.rb

--- lesson2/fibonacci.rb
+++ lesson2/fibonacci.rb
@@ -1,7 +1,7 @@
 array = [0, 1]
-loop do
-  i = array[-1] + array[-2]
-  break if i >= 100
-  array.push(i)
+number = 1
+while number <= 100
+  array.push(number)
+  number = array[-1] + array[-2]
 end
 print array
